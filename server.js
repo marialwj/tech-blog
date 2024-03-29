28a40c771dd9cf1d72610ddcddbd1e3a83fc086d
marialwj <mariaa.georgy@gmail.com>
2024-03-28 18:15:51 -0700

    controllers completed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utlils/helpers');
+const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({
     helpers
